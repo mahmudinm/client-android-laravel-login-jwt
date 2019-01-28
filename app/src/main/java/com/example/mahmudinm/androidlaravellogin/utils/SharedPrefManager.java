9446f7c06eca8f6cd7a760bb4c261f0a8ef8935a
mahmudin muttaqin <claxmanx@gmail.com>
2019-01-28 17:10:56 +0700

    buat login dan utils

---
+++ app/src/main/java/com/example/mahmudinm/androidlaravellogin/utils/SharedPrefManager.java
@@ -0,0 +1,49 @@
+package com.example.mahmudinm.androidlaravellogin.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPrefManager {
+
+    public static final String SP_LOGIN_APP = "spLoginApp";
+
+    public static final String SP_NAMA = "spNama";
+    public static final String SP_EMAIL = "spEmail";
+
+    public static final String SP_SUDAH_LOGIN = "spSudahLogin";
+
+    SharedPreferences sp;
+    SharedPreferences.Editor spEditor;
+
+    public SharedPrefManager(Context context){
+        sp = context.getSharedPreferences(SP_LOGIN_APP, Context.MODE_PRIVATE);
+        spEditor = sp.edit();
+    }
+
+    public void saveSPString(String keySP, String value){
+        spEditor.putString(keySP, value);
+        spEditor.commit();
+    }
+
+    public void saveSPInt(String keySP, int value){
+        spEditor.putInt(keySP, value);
+        spEditor.commit();
+    }
+
+    public void saveSPBoolean(String keySP, boolean value){
+        spEditor.putBoolean(keySP, value);
+        spEditor.commit();
+    }
+
+    public String getSPNama(){
+        return sp.getString(SP_NAMA, "");
+    }
+
+    public String getSPEmail(){
+        return sp.getString(SP_EMAIL, "");
+    }
+
+    public Boolean getSPSudahLogin(){
+        return sp.getBoolean(SP_SUDAH_LOGIN, false);
+    }
+}
